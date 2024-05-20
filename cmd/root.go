3d5b60816a4af5e6b098293583a0abf94b96d707
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2024-05-20 12:56:35 +0100

    cobra implementation with welcoming text

---
+++ cmd/root.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/charmbracelet/lipgloss"
+	"log"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "DotEM",
+	Short: ".EM is developer environment management tool",
+	Run: func(cmd *cobra.Command, args []string) {
+		var style = lipgloss.NewStyle().Foreground(lipgloss.Color("#FAFAFA")).Background(lipgloss.Color("#6b04fd")).Padding(2, 4, 2, 4).MarginBottom(2)
+		fmt.Println(style.Render(" __          __  _               z               _              ______ __  __ \n \\ \\        / / | |                            | |            |  ____|  \\/  |\n  \\ \\  /\\  / /__| | ___ ___  _ __ ___   ___    | |_ ___       | |__  | \\  / |\n   \\ \\/  \\/ / _ \\ |/ __/ _ \\| '_ ` _ \\ / _ \\   | __/ _ \\      |  __| | |\\/| |\n    \\  /\\  /  __/ | (_| (_) | | | | | |  __/   | || (_) |    _| |____| |  | |\n     \\/  \\/ \\___|_|\\___\\___/|_| |_| |_|\\___|    \\__\\___/    (_)______|_|  |_|\n                                                                             \n                                                                             "))
+
+		err := cmd.Help()
+		if err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
