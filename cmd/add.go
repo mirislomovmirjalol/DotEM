540d834352b762a5edfba7c2cb7673e358b18aa8
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2024-05-20 18:32:20 +0100

    feat: add command implement

---
+++ cmd/add.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github/mirislomovmirjalol/DotEM/internal/service"
+	"log"
+)
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add command used for adding new project to DotEM",
+	Args:  cobra.ExactArgs(2),
+	Run:   addProject,
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
+
+func addProject(cmd *cobra.Command, args []string) {
+	projectName := args[0]
+	filePath := args[1]
+
+	err := service.AddProject(projectName, filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
