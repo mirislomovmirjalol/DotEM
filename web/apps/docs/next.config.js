e79f3fae0bc1c29ff4ec4d5582c4487e73f595e5
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2024-05-21 15:15:57 +0100

    feat: docs setup

--- web/apps/docs/next.config.js
+++ web/apps/docs/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx'
+})
+
+module.exports = withNextra({
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
-};
+})
+
---
+++ web/apps/docs/theme.config.jsx
@@ -0,0 +1,6 @@
+export default {
+    logo: <h1 className="text-5xl font-bold">.EM Docs</h1>,
+    project: {
+        link: 'https://github.com/mirislomovmirjalol/DotEM'
+    }
+}
