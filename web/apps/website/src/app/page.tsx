872ddf57d031f518b37cb75ffe8bcff7a834e73d
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2024-05-21 17:15:56 +0100

    chore: web -> website, UI changes on website

--- web/apps/website/src/app/page.tsx
+++ web/apps/website/src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import {WelcomeSection} from "../components/welcome-section.tsx";
+import DotPattern from "../components/dot-pattern.tsx";
 
 export default function Page(): JSX.Element {
     return (
@@ -9,6 +10,7 @@ export default function Page(): JSX.Element {
                 <WelcomeSection/>
                 <Image alt="screenshot" className="mt-6" height={600} src="/screenshot.png" width={600}/>
             </section>
+            <DotPattern className="opacity-20" />
             <div className="absolute inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg -z-10"
                  style={{background: "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)"}}/>
         </main>
--- web/apps/website/src/components/navbar.tsx
+++ web/apps/website/src/components/navbar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Navbar(): JSX.Element {
     return (
-        <nav className="flex flex-row items-center justify-between w-screen p-4 border-b border-gray-800">
+        <nav className="fixed flex flex-row items-center justify-between w-screen p-4 border-b border-gray-800 backdrop-blur">
             <h1 className="text-primary px-3 py-1 rounded text-xl font-bold">.EM</h1>
             <div className="flex flex-row items-center space-x-4">
                 <Link href="https://github.com/mirislomovmirjalol/DotEM">
