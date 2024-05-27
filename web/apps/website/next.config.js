5aba7fe538557009e55c975315227f2440c62bf8
Jalol Mirislomov <mirislomovmirjalol@gmail.com>
2024-05-27 20:45:45 +0100

    chore: config files setup

--- web/apps/website/next.config.js
+++ web/apps/website/next.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
+  async redirects() {
+    return [
+      {
+        source: '/docs',
+        destination: 'http://docs.dotem.websit',
+        permanent: true,
+      },
+    ]
+  },
 };
